8bb53ebc60587ae3ff355753ade7629ddc1bb7c2
bakhshabadi <bakhshabadi.javad@gmail.com>
2021-09-05 07:31:25 +0430

    fix

--- dist/decorators/swg-get-all.decorator.d.ts
+++
@@ -1,3 +0,0 @@
-import { Type } from "@nestjs/common";
-export declare const ApiGetAll: <TModel extends Type<any>>(model: TModel, path?: string | undefined) => <TFunction extends Function, Y>(target: object | TFunction, propertyKey?: string | symbol | undefined, descriptor?: TypedPropertyDescriptor<Y> | undefined) => void;
-//# sourceMappingURL=swg-get-all.decorator.d.ts.map
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./services";
-export * from "./types";
-export * from "./decorators";
-//# sourceMappingURL=index.d.ts.map
