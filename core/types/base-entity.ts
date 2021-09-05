4bfd0ba22d5b6e4e1a2981b8b9b0151a16139282
bakhshabadi <bakhshabadi.javad@gmail.com>
2021-09-05 07:46:54 +0430

    add swagger in baseEntity

--- core/types/base-entity.ts
+++ core/types/base-entity.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 export class BaseEntity {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number=0;
 
+  @ApiProperty()
   @Column()
   insertedAt: Date=new Date();
 
+  @ApiProperty()
   @Column()
   updatedAt: Date=new Date();
 
