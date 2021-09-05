110698bed84c90f8723d044d9b8fd7d678e52404
bakhshabadi <bakhshabadi.javad@gmail.com>
2021-09-05 06:53:24 +0430

    add base entity and rename fix files

---
+++ types/base-entity.ts
@@ -0,0 +1,15 @@
+import {  Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number=0;
+  
+    @Column()
+    insertedAt: Date=new Date();
+
+    @Column()
+    updatedAt: Date=new Date();
+
+    @Column()
+    deletetAt: Date=new Date();
+}
