fb168d49c3f9be300f0c3432d4a718bfef790226
bakhshabadi <bakhshabadi.javad@gmail.com>
2021-09-05 07:31:10 +0430

    fix

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+export * from "./services";
+export * from "./types";
+export * from "./decorators";
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/services/index.d.ts
@@ -0,0 +1,2 @@
+export * from "./base.service";
+//# sourceMappingURL=index.d.ts.map
